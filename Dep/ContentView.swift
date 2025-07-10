bac7a1e0aab31a2e3a319f5862cade47d6c65798
Tatsuki ksto <tatsu.0329.gran.turismo@gmail.com>
2025-07-10 12:44:24 +0900

    Initial Commit

---
+++ Dep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dep
+//
+//  Created by Tatsuki Kato on 2025/07/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
